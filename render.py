62d2212d33b156952db320a02612ff234e217bd5
ioksengtan <ioksengtan@gmail.com>
2020-05-26 23:05:36 +0000

    20200527

--- render.py
+++ render.py
@@ -1,7 +1,20 @@
+svg_cmd_dict = [
+    'line', 'circle', 'rect'    
+]
+
 def render_svg(slef_dict, sdef_dict):
 	for instance in sdef_dict:
-		if 'layout' in sdef_dict[instance]:
-			for module in sdef_dict[instance]['layout']:
-				if module == 'line':
-					print sdef_dict[instance]['layout']['line']
+                print 'render:'+instance
+		        if 'layout' in sdef_dict[instance]:
+			        for module in sdef_dict[instance]['layout']:
+				        if module == 'line':
+					        print sdef_dict[instance]['layout']['line']
+                                        if module in svg_cmd_dict:
+                                                print 'svg native cmd: ' + module
+				                #print sdef_dict[instance]['layout'][module]
+                                        elif module in slef_dict:
+                                                print 'slef cmd: ' + module
+                                        else:
+                                                print 'cmd not support: ' + module
+                print 'render done'
 		
